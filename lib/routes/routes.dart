079a1117b711240418dc3c5dfcae59dfce24e40d
maheshbghule2001 <111970471+maheshbghule2000@users.noreply.github.com>
2025-01-20 15:48:15 +0530

    updated

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,25 +1,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:sd_maketing/routes/routes.gr.dart';
+
 @AutoRouterConfig(replaceInRouteName: 'Screen|View|Page,Route')
 class AppRouter extends RootStackRouter {
-
- AppRouter({
+  AppRouter({
     super.navigatorKey,
   });
-  
+
   @override
   List<AutoRoute> get routes => [
-    // HomeScreen is generated as HomeRoute because
-    // of the replaceInRouteName property
-    AutoRoute(page: DashboardRoute.page,initial: true),
-    AutoRoute(page: UserRoute.page),
-    // AutoRoute(page: DashboardViewRoute.page, initial: true),
-      // AutoRoute(page: DashboardViewRoute.page, initial: true),
-
-  ];
+        AutoRoute(page: DashboardRoute.page, initial: true),
+        AutoRoute(page: UserRoute.page),
+      ];
 
   @override
   List<AutoRouteGuard> get guards => [
-    // optionally add root guards here
-  ];
+        // optionally add root guards here
+      ];
 }
